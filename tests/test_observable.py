9d7cc9718026960a2ceebed0db789aca33929cc3
zeFresk <8807862+zeFresk@users.noreply.github.com>
2025-08-15 16:16:22 +0200

    Observable full testing suite

--- tests/test_observable.py
+++ tests/test_observable.py
@@ -1,6 +1,15 @@
 import pytest
-from pyrauli import Observable, PauliTerm, PauliGate, CliffordGate, CoefficientTruncator, from_qiskit
+from pyrauli import (
+    Observable,
+    PauliTerm,
+    PauliGate,
+    CliffordGate,
+    CoefficientTruncator,
+    UnitalNoise,
+    from_qiskit,
+)
 from math import pi
+
 
 def test_observable_init_string():
     obs = Observable("IIII")
@@ -22,37 +31,60 @@ def test_observable_init_pts():
     assert obs[0] == pt1
     assert obs[1] == pt2
 
+
 def test_empty_observable_not_allowed():
     with pytest.raises(ValueError):
         obs = Observable("")
+
 
 def test_mismatched_pt_not_allowed():
     with pytest.raises(ValueError):
         obs = Observable(["I", "XI"])
 
+
 def test_apply_pauli():
     obs = Observable("ZX")
     obs.apply_pauli(PauliGate.X, 0)
 
+
 def test_apply_clifford():
     obs = Observable("ZX")
     obs.apply_clifford(CliffordGate.H, 0)
+
 
 def test_apply_cx():
     obs = Observable("ZX")
     obs.apply_cx(0, 1)
     obs.apply_cx(1, 0)
 
+
 def test_apply_rz():
     obs = Observable("ZX")
-    obs.apply_rz(0, pi/2)
+    obs.apply_rz(0, pi / 2)
+
+
+def test_apply_amplitude_damping():
+    obs = Observable("ZX")
+    obs.apply_amplitude_damping(1, 0.01)
+
+
+def test_apply_unital_noise_depolarizing():
+    obs = Observable("ZX")
+    obs.apply_unital_noise(UnitalNoise.Depolarizing, 0, 0.01)
+
+
+def test_apply_unital_noise_dephazing():
+    obs = Observable("ZX")
+    obs.apply_unital_noise(UnitalNoise.Dephasing, 0, 0.01)
+
 
 def test_ev():
     obs = Observable("Z")
     obs.apply_clifford(CliffordGate.H, 0)
-    obs.apply_rz(0, pi/3)
+    obs.apply_rz(0, pi / 3)
     obs.apply_clifford(CliffordGate.H, 0)
     assert obs.expectation_value() == pytest.approx(0.5)
+
 
 def test_merge():
     obs = Observable(["IZ", "IZ"])
@@ -60,12 +92,37 @@ def test_merge():
     assert obs.size() == 1
     assert obs[0].coefficient == pytest.approx(2.0)
 
+
 def test_truncate():
     obs = Observable([PauliTerm("II", 0.999), PauliTerm("ZZ", 0.001)])
     truncator = CoefficientTruncator(0.01)
     obs.truncate(truncator)
     assert obs.size() == 1
     assert obs[0] == PauliTerm("II", 0.999)
+
+
+def test_bad_op_not_allowed():
+    obs = Observable("ZZ")
+    with pytest.raises(ValueError):
+        obs.apply_pauli(PauliGate.X, 2)
+    with pytest.raises(ValueError):
+        obs.apply_clifford(CliffordGate.H, 2)
+    with pytest.raises(ValueError):
+        obs.apply_cx(0, 2)
+    with pytest.raises(ValueError):
+        obs.apply_cx(2, 0)
+    with pytest.raises(ValueError):
+        obs.apply_cx(0, 0)
+    with pytest.raises(ValueError):
+        obs.apply_rz(2, pi / 2)
+    with pytest.raises(ValueError):
+        obs.apply_amplitude_damping(2, 0.01)
+    with pytest.raises(ValueError):
+        obs.apply_unital_noise(UnitalNoise.Depolarizing, 2, 0.01)
+
+
+def test_len():
+    assert len(Observable(["II", "XX"])) == 2
 
 
 # --- Optional Qiskit Integration Tests ---
