a8d4cb668026b2efa3f48906a857d8074f7c4793
zeFresk <8807862+zeFresk@users.noreply.github.com>
2025-08-16 18:37:06 +0200

    fix qiskit benchmarks + fix bug in _run_job

--- benchmarks/test_benchmark_qiskit.py
+++ benchmarks/test_benchmark_qiskit.py
@@ -25,16 +25,25 @@ def qiskit_resources():
 
     # For circuit conversion
     qiskit_circ_convert = QuantumCircuit(32)
-    for i in range(32):
+    for i in range(32*32):
         gate = np.random.choice(["H", "CX", "Rz"])
         if gate == "H":
-            qiskit_circ_convert.add_operation("H", qubit=(i % 32))
+            qiskit_circ_convert.h(i % 32)
         elif gate == "CX":
-            qiskit_circ_convert.add_operation("CX", control=(i % 32), target=((i + 1) % 32))
+            qiskit_circ_convert.cx(i % 32, np.random.choice([k for k in range(32) if k != (i%32)]))
         elif gate == "Rz": # Rz
-            qiskit_circ_convert.add_operation("Rz", qubit=(i % 32), param=np.random.rand())
+            qiskit_circ_convert.rz(np.random.rand() * np.pi, i % 32)
         else:
-            qiskit_circ_convert.add_operation(np.random.choice(["I", "X", "Y", "Z"]), qubit=(i % 32))
+            pg = np.random.choice(["I", "X", "Y", "Z"])
+            qubit= i % 32
+            if pg == "I":
+                qiskit_circ_convert.i(qubit)
+            elif pg == "X":
+                qiskit_circ_convert.x(qubit)
+            elif pg == "Y":
+                qiskit_circ_convert.y(qubit)
+            elif pg == "Z":
+                qiskit_circ_convert.z(qubit)
 
     return {
         "qiskit_obs": qiskit_obs,
@@ -43,46 +52,43 @@ def qiskit_resources():
 
 # --- Qiskit Integration Benchmarks ---
 
-def benchmark_qiskit_observable_conversion(benchmark, qiskit_resources):
+def test_qiskit_1024observable_conversion(qiskit_resources, benchmark):
     q_obs = qiskit_resources["qiskit_obs"]
     benchmark(from_qiskit, q_obs)
 
-def benchmark_qiskit_circuit_conversion(benchmark, qiskit_resources):
+def test_qiskit_32x32circuit_conversion(qiskit_resources, benchmark):
     q_circ = qiskit_resources["qiskit_circ_convert"]
     benchmark(from_qiskit, q_circ)
 
-def benchmark_qiskit_pbackend_run(benchmark):
+def test_qiskit_pbackend_run8x8(benchmark):
     np.random.seed(42)
-    backend = PBackend(num_qubits=16)
-    pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
-    qiskit_circ_run_raw = random_circuit(16, 16, seed=42, max_operands=2)
+    backend = PBackend(num_qubits=8)
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=0)
+    qiskit_circ_run_raw = random_circuit(8, 8, seed=42, max_operands=2)
     qiskit_circ_run_transpiled = pm.run(qiskit_circ_run_raw)
-    run_obs = SparsePauliOp("Z" * 16)
+    run_obs = SparsePauliOp("Z" * 8)
     def run_job():
         job = backend.run([(qiskit_circ_run_transpiled, run_obs)])
         job.result()
         
     benchmark(run_job)
 
-def benchmark_qiskit_pbackend_parameterized_run(benchmark):
+def test_qiskit_pbackend_parameterized_zz_real_amp8x8(benchmark):
     np.random.seed(42)
-    num_qubits_param = 16
+    num_qubits_param = 8
     feature_map = ZZFeatureMap(feature_dimension=num_qubits_param, reps=1)
-    ansatz = RealAmplitudes(num_qubits=num_qubits_param, reps=1)
+    ansatz = RealAmplitudes(num_qubits=8, reps=1)
     param_qc = feature_map.compose(ansatz)
     param_obs = SparsePauliOp("Z" * num_qubits_param)
     param_vals = np.random.rand(param_qc.num_parameters)
-
-    # Setup estimator with noise and truncation
-    noise_model = NoiseModel()
-    noise_model.add_unital_noise_on_all_gates(UnitalNoise.Depolarizing, 0.01)
+    backend = PBackend(num_qubits=8)
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=0)
+    isa_qc = pm.run(param_qc)
     
-    estimator = PyrauliEstimator(
-        noise_model=noise_model,
-    )
+    estimator = PyrauliEstimator()
     
     def run_job():
-        job = estimator.run([(param_qc, param_obs, param_vals)])
+        job = estimator.run([(isa_qc, param_obs, param_vals)])
         job.result()
         
     benchmark(run_job)
