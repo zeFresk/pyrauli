bf573d92f75bb58f83f41dc1c46cc0374d7dbe7a
zeFresk <8807862+zeFresk@users.noreply.github.com>
2026-03-12 17:53:48 +0100

    fix compilation error

--- src/pyrauli/converters.py
+++ src/pyrauli/converters.py
@@ -56,7 +56,7 @@ def from_qiskit_qc(qiskit_circuit: QuantumCircuit, noise_model: pyrauli.NoiseMod
             target = qubits[0]
             
             pyrauli_circuit.h(target)
-            pyrauli_circuit.rz(theta, target)
+            pyrauli_circuit.rz(target, theta)
             pyrauli_circuit.h(target)
         elif op_name == "ry":
             # Decompose Ry(theta) -> Rz(-pi/2) * H * Rz(-theta) * H * Rz(pi/2)
@@ -64,15 +64,15 @@ def from_qiskit_qc(qiskit_circuit: QuantumCircuit, noise_model: pyrauli.NoiseMod
             target = qubits[0]
             
             # 1. Rz(pi/2)
-            pyrauli_circuit.rz(math.pi / 2, target)
+            pyrauli_circuit.rz(target, math.pi / 2)
             # 2. H
             pyrauli_circuit.h(target)
             # 3. Rz(-theta)
-            pyrauli_circuit.rz(-theta, target)
+            pyrauli_circuit.rz(target, -theta)
             # 4. H
             pyrauli_circuit.h(target)
             # 5. Rz(-pi/2)
-            pyrauli_circuit.rz(-math.pi / 2, target)
+            pyrauli_circuit.rz(target, -math.pi / 2)
         elif op_name == "cz":
             # Decompose CZ(c, t) -> H(t) CX(c, t) H(t)
             control, target = qubits[0], qubits[1]
@@ -83,13 +83,13 @@ def from_qiskit_qc(qiskit_circuit: QuantumCircuit, noise_model: pyrauli.NoiseMod
             # SQRT-X -> H * Rz(pi/2) * H
             target = qubits[0]
             pyrauli_circuit.h(target)
-            pyrauli_circuit.rz(math.pi / 2, target)
+            pyrauli_circuit.rz(target, math.pi / 2)
             pyrauli_circuit.h(target)
         elif op_name == "sxdg": 
             # Inverse Sqrt-X -> H * Rz(-pi/2) * H
             target = qubits[0]
             pyrauli_circuit.h(target)
-            pyrauli_circuit.rz(-math.pi / 2, target)
+            pyrauli_circuit.rz(target, -math.pi / 2)
             pyrauli_circuit.h(target)
         elif op_name == "iswap":
             q0, q1 = qubits[0], qubits[1]
@@ -105,15 +105,15 @@ def from_qiskit_qc(qiskit_circuit: QuantumCircuit, noise_model: pyrauli.NoiseMod
             pyrauli_circuit.h(q1)
             
             # 3. Apply S gates (S = Rz(pi/2))
-            pyrauli_circuit.rz(math.pi / 2, q0)
-            pyrauli_circuit.rz(math.pi / 2, q1)
+            pyrauli_circuit.rz(q0, math.pi / 2)
+            pyrauli_circuit.rz(q1, math.pi / 2)
         elif op_name == "rzz":
             # Decompose Rzz(theta) -> CX(0,1) -> Rz(theta) on target -> CX(0,1)
             theta = float(params[0])
             q0, q1 = qubits[0], qubits[1]
 
             pyrauli_circuit.cx(q0, q1)
-            pyrauli_circuit.rz(theta, q1)
+            pyrauli_circuit.rz(q1, theta)
             pyrauli_circuit.cx(q0, q1)
 
         elif op_name not in ["measure", "barrier"]:
