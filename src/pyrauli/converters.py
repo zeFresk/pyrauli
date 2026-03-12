76c0bcdb9808af931d23df19ec1e373e373b90a2
zeFresk <8807862+zeFresk@users.noreply.github.com>
2026-03-12 16:46:37 +0100

    improved `from_qiskit_qc` to handle more gates

--- src/pyrauli/converters.py
+++ src/pyrauli/converters.py
@@ -6,6 +6,7 @@ import logging
 from qiskit.circuit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 import pyrauli
+import math
 
 def from_qiskit(qiskit_obj, noise_model: pyrauli.NoiseModel = None, reverse=False):
     """
@@ -36,14 +37,85 @@ def from_qiskit_qc(qiskit_circuit: QuantumCircuit, noise_model: pyrauli.NoiseMod
     for instruction in qiskit_circuit.data:
         op_name = instruction.operation.name.lower()
         qubits = [qiskit_circuit.find_bit(q).index for q in instruction.qubits]
+        params = instruction.operation.params
 
-        if op_name == "h":
-            pyrauli_circuit.add_operation("H", qubits[0])
+        if op_name in ["h", "x", "y", "z"]:
+            getattr(pyrauli_circuit, op_name)(qubits[0])
         elif op_name == "cx":
             pyrauli_circuit.add_operation("CX", qubits[0], qubits[1])
         elif op_name == "rz":
-            theta = instruction.operation.params[0]
-            pyrauli_circuit.add_operation("Rz", qubits[0], float(theta))
+            pyrauli_circuit.add_operation("Rz", qubits[0], float(params[0]))
+        elif op_name in ["u", "u3"]:
+            theta, phi, lam = map(float, params)
+            pyrauli_circuit.add_operation("u3", qubits[0], theta, phi, lam)
+            
+        # --- Decompositions ---
+        elif op_name == "rx":
+            # Decompose Rx(theta) -> H * Rz(theta) * H
+            theta = float(params[0])
+            target = qubits[0]
+            
+            pyrauli_circuit.h(target)
+            pyrauli_circuit.rz(theta, target)
+            pyrauli_circuit.h(target)
+        elif op_name == "ry":
+            # Decompose Ry(theta) -> Rz(-pi/2) * H * Rz(-theta) * H * Rz(pi/2)
+            theta = float(params[0])
+            target = qubits[0]
+            
+            # 1. Rz(pi/2)
+            pyrauli_circuit.rz(math.pi / 2, target)
+            # 2. H
+            pyrauli_circuit.h(target)
+            # 3. Rz(-theta)
+            pyrauli_circuit.rz(-theta, target)
+            # 4. H
+            pyrauli_circuit.h(target)
+            # 5. Rz(-pi/2)
+            pyrauli_circuit.rz(-math.pi / 2, target)
+        elif op_name == "cz":
+            # Decompose CZ(c, t) -> H(t) CX(c, t) H(t)
+            control, target = qubits[0], qubits[1]
+            pyrauli_circuit.h(target)
+            pyrauli_circuit.cx(control, target)
+            pyrauli_circuit.h(target)
+        elif op_name == "sx":
+            # SQRT-X -> H * Rz(pi/2) * H
+            target = qubits[0]
+            pyrauli_circuit.h(target)
+            pyrauli_circuit.rz(math.pi / 2, target)
+            pyrauli_circuit.h(target)
+        elif op_name == "sxdg": 
+            # Inverse Sqrt-X -> H * Rz(-pi/2) * H
+            target = qubits[0]
+            pyrauli_circuit.h(target)
+            pyrauli_circuit.rz(-math.pi / 2, target)
+            pyrauli_circuit.h(target)
+        elif op_name == "iswap":
+            q0, q1 = qubits[0], qubits[1]
+            
+            # 1. Apply SWAP (Standard 3-CNOT decomposition)
+            pyrauli_circuit.cx(q0, q1)
+            pyrauli_circuit.cx(q1, q0)
+            pyrauli_circuit.cx(q0, q1)
+            
+            # 2. Apply CZ -> H(1) CX(0,1) H(1)
+            pyrauli_circuit.h(q1)
+            pyrauli_circuit.cx(q0, q1)
+            pyrauli_circuit.h(q1)
+            
+            # 3. Apply S gates (S = Rz(pi/2))
+            pyrauli_circuit.rz(math.pi / 2, q0)
+            pyrauli_circuit.rz(math.pi / 2, q1)
+        elif op_name == "rzz":
+            # Decompose Rzz(theta) -> CX(0,1) -> Rz(theta) on target -> CX(0,1)
+            theta = float(params[0])
+            q0, q1 = qubits[0], qubits[1]
+
+            pyrauli_circuit.cx(q0, q1)
+            pyrauli_circuit.rz(theta, q1)
+            pyrauli_circuit.cx(q0, q1)
+
         elif op_name not in ["measure", "barrier"]:
             raise NotImplementedError(
                 f"Gate '{op_name}' is not supported by the converter."
